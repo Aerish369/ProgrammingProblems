df982f88d28d4aa06387a230a0d4d717f8d8aff9
Aerish369 <aryalaerish369@gmail.com>
2023-07-10 08:03:22 +0545

    2023.7.10

---
+++ Daily Problems/String to array/main.py
@@ -0,0 +1,15 @@
+# Write a function to split a string and convert it into an array of words.
+
+# Examples (Input ==> Output):
+# "Robin Singh" ==> ["Robin", "Singh"]
+
+# "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+def string_to_array(s):
+    a = []
+    for i in s.split(' '): a.append(i) 
+    return a
+
+
+# Example usecase
+print(string_to_array('Robin Chan is cutie'))
