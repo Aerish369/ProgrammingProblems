6acb4c8caadd604fbc85602061d299ee17f2160a
Aerish369 <aryalaerish369@gmail.com>
2023-08-03 16:21:40 +0545

    2023.8.3

---
+++ Daily Problems/remove exclamation/main.py
@@ -0,0 +1,9 @@
+
+#!Write a python program to remove all the exclamation signs from a given string.abs
+
+
+def removeexclamtn(s):
+    return s.replace('!', '')
+
+
+print(removeexclamtn("!!YOYO Bantai Rapper!!!"))
