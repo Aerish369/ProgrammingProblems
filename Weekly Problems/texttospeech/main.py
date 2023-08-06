a1b904624e9e8518cf2653dff5ce7707aafeb607
Aerish369 <aryalaerish369@gmail.com>
2023-08-06 18:47:15 +0545

    2nd commit for exercise 9

--- Weekly Problems/texttospeech/main.py
+++ Weekly Problems/texttospeech/main.py
@@ -1,3 +1,15 @@
+# Write a program to pronounce list of names using win32 API. If you are given a list l as follows:
+
+# l = ["Rahul", "Nishant", "Harry"]
+
+
+# Your program should pronouce:
+
+# Shoutout to Rahul
+# Shoutout to Nishant
+# Shoutout to Harry
+
+
 import win32com.client as wincl
 
 
